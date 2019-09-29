9f7e457e50cfcec45eb095ba9ddbc323be051b65
Sebastian <sebastian@jskramer.de>
2019-09-29 18:40:24 +0200

    Fixing stuff to update to laravel 6.*

--- src/Vatsim/Xml/XML.php
+++ src/Vatsim/Xml/XML.php
@@ -1,34 +1,57 @@
-<?php namespace Vatsim\Xml;
+<?php
 
-use \SimpleXMLElement;
+namespace Vatsim\Xml;
 
-class XML {
+use SimpleXMLElement;
+
+class XML
+{
     private $_urls = [];
-    public function __construct($urls){
+
+    /**
+     * Initiate the Module.
+     *
+     * @param [type] $urls [description]
+     */
+    public function __construct($urls)
+    {
         $this->_urls = $urls;
     }
-    
-    public function getData($cid, $url='idstatus'){
-        if(!array_key_exists($url, $this->_urls)){
-            return array();
+
+    /**
+     * Grab data for a VID from the datastream and return the resulting response.
+     *
+     * @param [type] $cid [description]
+     * @param string $url [description]
+     *
+     * @return [type] [description]
+     */
+    public function getData($cid, $url = 'idstatus')
+    {
+        if (!array_key_exists($url, $this->_urls)) {
+            return [];
         }
-        
+
         // let's load the URL!
-        $context = stream_context_create(['ssl' => [
-                'verify_peer' => true,
-                'allow_self_signed' => true,
-                'CN_match' => 'cert.vatsim.net',
-        ]]);
+        $context = stream_context_create(
+            ['ssl' => [
+                    'verify_peer' => true,
+                    'allow_self_signed' => true,
+                    'CN_match' => 'cert.vatsim.net',
+                ],
+            ]
+        );
         $url = sprintf($this->_urls[$url], $cid);
         $xml = new SimpleXMLElement(file_get_contents($url, false, $context));
-        
-        if(!isset($xml->user) OR !isset($xml->user[0])){
-            throw new VatsimXMLExpception("Invalid response received: ".$url);
+
+        if (!isset($xml->user) or !isset($xml->user[0])) {
+            throw new VatsimXMLExpception('Invalid response received: '.$url);
         }
-        
+
         $values = json_decode(json_encode($xml->user), false);
-        $values->cid = $values->{"@attributes"}->cid;
-        unset($values->{"@attributes"});
+        $values->cid = $values->{'@attributes'}->cid;
+        unset($values->{'@attributes'});
+
         return $values;
     }
 }
--- src/Vatsim/Xml/XmlServiceProvider.php
+++ src/Vatsim/Xml/XmlServiceProvider.php
@@ -1,67 +1,69 @@
-<?php namespace Vatsim\Xml;
+<?php
+
+namespace Vatsim\Xml;
 
 use Illuminate\Support\ServiceProvider;
 
-class XmlServiceProvider extends ServiceProvider {
+class XmlServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-	
-	public function dir($path){
-		return __DIR__."/../../".$path;
-	}
+    public function dir($path)
+    {
+        return __DIR__.'/../../'.$path;
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+    }
 
-	}
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $request = $this->app['request'];
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$request = $this->app['request'];
-
-		// Lumen users need to copy the config themselves
-		// And it needs to pulled completely differently.
-		// So more work required. Luckily barryvdh had the answer - so thanks.
-		if(str_contains($this->app->version(), "Lumen")){
-			$this->app->configure("vatsim-xml");
-		} else {
-			$this->publishes([
-				$this->dir("config/config.php") => config_path("vatsim-xml.php"),
-			]);
+        // Lumen users need to copy the config themselves
+        // And it needs to pulled completely differently.
+        // So more work required. Luckily barryvdh had the answer - so thanks.
+        if (str_contains($this->app->version(), 'Lumen')) {
+            $this->app->configure('vatsim-xml');
+        } else {
+            $this->publishes(
+                [
+                    $this->dir('config/config.php') => config_path('vatsim-xml.php'),
+                ]
+            );
 
             $this->mergeConfigFrom(
-                __DIR__ . '/../../config/config.php', 'vatsim-xml'
+                __DIR__.'/../../config/config.php', 'vatsim-xml'
             );
-		}
+        }
 
-		$this->app->singleton('vatsimxml', function($app){
-			return new XML( $this->app["config"]->get("vatsim-xml") );
-		});
-	}
+        $this->app->singleton(
+            'vatsimxml',
+            function ($app) {
+                return new XML($this->app['config']->get('vatsim-xml'));
+            }
+        );
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [XML::class];
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [XML::class];
+    }
 }
